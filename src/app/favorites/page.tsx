e86cc2aaf8ad64f3a34ba66dee8eba99a145d632
Benzagon <gonzabenzaquen@gmail.com>
2024-03-26 17:18:57 -0300

    feat: saved cities title

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -2,7 +2,8 @@ import FavoritesGrid from '../../components/favorites/grid';
 
 export default function Favorites() {
     return (
-      <div className='h-screen w-screen flex items-start p-20 justify-center'>
+      <div className='h-screen w-screen flex flex-col items-center p-20 pt-32 gap-8 justify-start'>
+        <h1 className="text-2xl text-center font-medium w-full">Saved cities</h1>
         <FavoritesGrid/>
       </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Indexbody from "@/components/main/IndexBody";
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 
 export default function Home() {
   return (
-    <div className='h-screen w-screen flex items-start p-20 justify-center'>
+    <div className='h-screen w-screen flex flex-col gap-8 items-center p-20 justify-start'>
+      <div className="flex justify-center items-center gap-2 h-fit">
+                <ThunderstormIcon className="text-blue-500"></ThunderstormIcon>
+                <h1 className="text-2xl text-center font-medium">Weatherly!</h1>
+            </div>
       <Indexbody></Indexbody>
     </div>
   );
